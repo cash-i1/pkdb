2a1419b1f5ab45ee5321b3c00e0a6ca458977049
c <cashignitro@gmail.com>
2024-10-26 12:36:31 +1100

    added `Card` and loading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::fs;
+
+use yaml_rust2::{YamlEmitter, YamlLoader};
+
+#[derive(Debug)]
+struct ImageName(String);
+
+#[derive(Debug)]
+struct Card {
+    name: String,
+    img: ImageName,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut cards: Vec<Card> = vec![];
+
+    let cards_yaml = fs::read_dir("./data/cards").unwrap();
+    for card in cards_yaml {
+        if let Ok(card) = card {
+            let contents = fs::read_to_string(card.path()).unwrap();
+            let yaml = YamlLoader::load_from_str(&contents).unwrap()[0].clone();
+
+            cards.push(Card {
+                name: yaml["name"].as_str().unwrap().to_string(),
+                img: ImageName(yaml["img"].as_str().unwrap().to_string()),
+            });
+        }
+    }
+    
+    for card in cards {
+        println!("{:?}", card);
+    }
+
 }
